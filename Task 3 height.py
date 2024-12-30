03e1a8997d853c432399daa0e244d45ce28b380f
divii15 <divyadaksha143@gmail.com>
2024-12-30 21:25:49 +0530

    My next task(3)(5) 30/12

---
+++ Task 3 height.py
@@ -0,0 +1,9 @@
+height=int(input("Enter the value:"))
+  if(height<150):
+     print("Output will be small")
+  elif(height>=150 and height<165):
+     print("Output will be average height")
+   elif(height>=165 and height<195):
+     print("Output will be Taller")
+   else: 
+     print("Output will be abnormal height")
