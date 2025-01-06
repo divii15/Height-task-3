3d77dd6059cd61e2c19e0b1258b0427921a50c87
divii15 <divyadaksha143@gmail.com>
2025-01-06 17:27:41 +0530

    My next task(3)(5)

--- Task 3 height.py
+++ Task 3 height.py
@@ -1,3 +1,13 @@
+#TASK 3
+#5)Get the height from user.
+#if given height is less than 150, output will be small.
+#if given height is greater than or equal 150 and height less than 165, output will be Average Height.
+#if given height is greater than or equal 165 and height less than 195, output will be  Taller.
+#otherwise output will be Abnormal height.
+
+
+
+
 height=int(input("Enter the value:"))
   if(height<150):
      print("Output will be small")
